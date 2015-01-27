49767e550aa2614e63aea0798c266138d4aa2f0c
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-27 09:38:35 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,73 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+emily = User.create(
+	name: "emily"
+	,uid: "111"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+
+sung = User.create(
+	name: "sung"
+	,uid: "222"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+
+rebecca = User.create(
+	name: "rebecca"
+	,uid: "333"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+
+james = User.create(
+	name: "james"
+	,uid: "444"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+
+jesse = User.create(
+	name: "jesse"
+	,uid: "555"
+	,provider:  "twitter"
+	,oauth_callback_confirmed: true
+	)
+
+sean = User.create(
+	name: "sean"
+	,uid: "666"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+
+pam = User.create(
+	name: "pam"
+	,uid: "777"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+
+obama = User.create(
+	name: "obama"
+	,uid: "888"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+biden = User.create(
+	name: "joebiden"
+	,uid: "999"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+bieber = User.create(
+	name: "bieber"
+	,uid: "100"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
+santa = User.create(
+	name: "santa"
+	,uid: "200"
+	,provider: "twitter"
+	,oauth_callback_confirmed: true
+	)
