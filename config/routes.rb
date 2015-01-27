dea85931d09bfe2e5bf098152f3a13cb37697477
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-27 10:22:26 -0500

    integrating changes. requires db reset

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,10 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index] #for user stats and leaderboard
   resources :hashtags, only: [:index] #for hashtag leaderboard
 
-
-
-  resources :application
-
-
- 
+  resources :application, only: [:create]
 
   match "auth/:provider/callback" => "sessions#create" , via: [:get, :post]
   match "/signout" => "sessions#destroy", via: [:get, :post], :as => "signout"
 
 
-
-
- 
 end
