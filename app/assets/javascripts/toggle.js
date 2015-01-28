ae220ecdb3833eff9b14e5dd7e98d6345123ef1c
James Corey <mmjames04@yahoo.com>
2015-01-28 17:50:22 -0500

    ajax request for leaderboard and stats

--- app/assets/javascripts/toggle.js
+++
@@ -1,7 +0,0 @@
-$('#leaderboard_link').unbind("click").on('click', function(){
-	$('#leaderboard').toggle('slow');
-});
-
-$('#your_stats').unbind('click').on('click', function(){
-	$('#stats').toggle('slow');
-});
