109af5c92f95248b13683497b6afc44f55763fe1
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-29 21:18:11 -0500

    commenting out circle explode js

--- app/assets/javascripts/circle.js
+++ app/assets/javascripts/circle.js
@@ -1,8 +1,8 @@
 
 
-$(document).ready(function() {
-	$('#circle-one').on('click', function(){
-		alert("explode");
-		$('#circle-one').toggle('explode', {pieces: 500}, 1000);
-	})
-});
+// $(document).ready(function() {
+// 	$('#circle-one').on('click', function(){
+// 		alert("explode");
+// 		$('#circle-one').toggle('explode', {pieces: 500}, 1000);
+// 	})
+// });
